3bbfb29091fdc92bf0470bcf9002436f9af081c4
Alessio Minichiello <minichielloalessio@gmail.com>
2018-12-21 15:02:00 +0000

    Updates

---
+++ precache-manifest.0d58b90852013dae3a64cc4aa3956bfe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9321cfb725142c9a8586",
+    "url": "/react-swapi-hooks/static/js/runtime~main.9321cfb7.js"
+  },
+  {
+    "revision": "71f015c8240844aafce9",
+    "url": "/react-swapi-hooks/static/js/main.71f015c8.chunk.js"
+  },
+  {
+    "revision": "c4182fad3afec5890aa3",
+    "url": "/react-swapi-hooks/static/js/1.c4182fad.chunk.js"
+  },
+  {
+    "revision": "fe158990ea2141917ad6160bfbd4b907",
+    "url": "/react-swapi-hooks/index.html"
+  }
+];
